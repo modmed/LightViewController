fba5dc4fde41b031695356f38002e1d3af901e72
Ashraf Jaddo <ashraf.jaddo@modernizingmedicine.com>
2017-06-16 14:18:08 -0400

    Initial Commit

---
+++ LightViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightViewController
+//
+//  Created by Ashraf Jaddo on 6/16/17.
+//  Copyright © 2017 Modernizing Medicine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
